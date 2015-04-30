802f6ff9f0f648099142a64d103068d49c41e3ac
BuildTools <unconfigured@null.spigotmc.org>
2015-04-30 15:06:24 +0200

    creadas clases y metodos del diagrama UML

---
+++ NewsFeed.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class NewsFeed here.
+ * 
+ * @author (Josu) 
+ * @version (30/04/2015)
+ */
+public class NewsFeed
+{
+    private ArrayList<MessagePost> messages;
+    private ArrayList<PhotoPost> photos; 
+
+    /**
+     * Constructor for objects of class NewsFeed
+     */
+    public NewsFeed()
+    {
+       messages = new ArrayList<>();
+       photos = new ArrayList<>();
+    }
+
+    
+    public void addMessagePost(MessagePost message)
+    {
+        
+    }
+    
+    public void addPhotoPost(PhotoPost photo)
+    {
+    
+    }
+    
+    public void show()
+    {
+    
+    }
+}
---
+++ PhotoPost.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class PhotoPost here.
+ * 
+ * @author (Josu) 
+ * @version (30/04/2015)
+ */
+public class PhotoPost
+{
+    private String userName;
+    private String fileName;
+    private String caption;
+    private long timeStamp;
+    private int likes;
+    private ArrayList<String> comments;
+
+    /**
+     * Constructor for objects of class PhotoPost
+     */
+    public PhotoPost(String author, String fileName, String caption)
+    {
+       userName = author;
+       this.fileName = fileName;
+       this.caption = caption;
+       timeStamp = 0;
+       likes = 0;
+       comments = new ArrayList<>();
+    }
+    
+    public void like()
+    {
+    
+    }
+    
+    public void unLike()
+    {
+    
+    }
+    
+    public void addComment(String text)
+    {
+    
+    }
+    
+    public String getImageFile()
+    {
+        String imageFile = "";
+        return imageFile;
+    }
+    
+    public String getCaption()
+    {
+        return caption;
+    }
+    
+    public long getTimeStamp()
+    {
+        return timeStamp;
+    }
+    
+    public void display()
+    {
+        
+    }
+    
+    public long timeString(long time)
+    {
+        long timeString = 0;
+        return timeString;
+    }
+}
